c6f0564eac33d626d01c96669fd4f2a12666112c
Connor Allen <connor@zenrs.com>
2021-09-20 11:48:59 +0100

    =SocketService Added

--- searchio-backend/controllers/api.ts
+++ searchio-backend/controllers/api.ts
@@ -1,5 +1,16 @@
 import { Router, Request, Response } from 'express';
 export const router = Router();
+import { SocketService } from '../modules/SocketService';
 
 //  Simple test endpoint to see if the RESTful API is currently queriable
 router.get('/test/', async (req: Request, res: Response) => { return res.status(200).json({ success: true, message: 'Successful API test!'}) });
+
+
+
+
+const socket = new SocketService();
+
+
+setTimeout(() => {
+    console.log(socket.getID(), socket.isConnected());
+}, 1000);
