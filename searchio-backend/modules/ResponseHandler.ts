b8dedec45b206fb11b6d2ed294d6b6fb2029429a
Connor Allen <connor@zenrs.com>
2021-09-28 12:38:37 +0100

    =Merge cleanup

--- searchio-backend/modules/ResponseHandler.ts
+++ searchio-backend/modules/ResponseHandler.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { SearchioResponse } from "../models/SearchioResponse";
 
 export function success(message: string, data: any = undefined): SearchioResponse {
@@ -7,14 +6,4 @@ export function success(message: string, data: any = undefined): SearchioRespons
 
 export function error(message: string, data: any = undefined): SearchioResponse {
     return { success: false, message: `ERROR: ${message}`, data: data }
-=======
-import { SearchioResponse } from "../models/SearchioResponse";
-
-export function success(message: string, data: any = undefined): SearchioResponse {
-    return { success: true, message: message, data: data }
-}
-
-export function error(message: string, data: any = undefined): SearchioResponse {
-    return { success: false, message: `ERROR: ${message}`, data: data }
->>>>>>> origin/scraper-stream
 }
