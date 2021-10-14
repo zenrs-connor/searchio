814039919bcd242585033054e3ee924aae59469d
unknown <thom@zenrs.com>
2021-10-14 12:44:51 +0100

    =Added name search to companies house

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -1,15 +1,16 @@
+import { Stream } from "./modules/streams/Stream";
 import { CompaniesHouseStream } from "./modules/streams/CompaniesHouseStream";
 
 
-export class tomSandbox {
+export class tomSandbox extends Stream {
 
     constructor() {
-
+        super('',undefined);
     };
 
     public async run() {
-        let x = new CompaniesHouseStream('');
-        x.nameSearch();
+        let x = new CompaniesHouseStream('', undefined);
+        x.nameSearch('Elliott Phillips');
     }
 
 }
