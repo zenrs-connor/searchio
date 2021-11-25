5b6ebabf52c73049f7d921943f7c65e0aea3f2ce
unknown <thom@zenrs.com>
2021-11-25 09:59:21 +0000

    =DOAJ complete

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,20 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { DOAJSearch } from "./modules/processes/DOAJ/DOAJSearch";
 
+
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new DOAJSearch(socket, "CPR");
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+    console.log(y.data.rows.length);
+
+}
