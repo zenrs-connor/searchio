209e32b91d36df9813edc0fdfc6d3c3f6f1b39e3
unknown <thom@zenrs.com>
2021-11-25 11:08:41 +0000

    =ERIC complete

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,19 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { ERICSearch } from "./modules/processes/ERIC/ERICSearch";
 
+
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new ERICSearch(socket, "CPR");
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+
+}
