25395a557772ad00aff90911e9fb71798753861f
unknown <thom@zenrs.com>
2022-01-12 14:37:19 +0000

    =FCA searches complete

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,20 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { FCAFirmSearch } from "./modules/processes/FCA/FCAFirmSearch";
+import { FCAIndividualSearch } from "./modules/processes/FCA/FCAIndividualSearch";
 
+
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new FCAIndividualSearch(socket, "James Smith");
+    
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+}
