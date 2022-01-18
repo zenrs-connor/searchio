0f5c878f42c388b86460669e7bb4b72a60d1639e
unknown <thom@zenrs.com>
2022-01-18 15:30:40 +0000

    =Insolvency service individual and trading name searches complete

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,18 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { InsolvencyServiceSearchFirms } from "./modules/processes/InsolvencyService/InsolvencyServiceSearchFirms";
 
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new InsolvencyServiceSearchFirms(socket, "T");
+    
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+}
