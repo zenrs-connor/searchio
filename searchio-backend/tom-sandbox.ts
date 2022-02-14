c363da280eafa82bf92a32a5336004408a0fced7
unknown <thom@zenrs.com>
2022-02-14 10:09:54 +0000

    =EmailRep not finished, hit API limit, so waiting

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,18 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { EmailRepSearch } from "./modules/processes/EmailRep/EmailRepSearch";
 
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new EmailRepSearch(socket, "Elljp33@gmail.com");
+    
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+}
