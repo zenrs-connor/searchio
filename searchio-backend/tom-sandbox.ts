8c5c14ff1a716f476a5c55c7967ccfe769f2acc4
unknown <thom@zenrs.com>
2022-03-31 15:18:42 +0100

    =Etherscan complete

--- searchio-backend/tom-sandbox.ts
+++ searchio-backend/tom-sandbox.ts
@@ -7,4 +7,18 @@ import { HunterEmailSearch } from "./modules/processes/Hunter/HunterEmailSearch"
 import { IPAPISearch } from "./modules/processes/IPAPI/IPAPISearch";
 import { MailBoxLayerSearch } from "./modules/processes/MailBoxLayer/MailBoxLayerSearch";
 import { Process } from "./modules/processes/Process";
+import { SocketService } from "./modules/SocketService";
+import { EtherscanSearch } from "./modules/processes/Etherscan/EtherscanSearch";
 
+export async function run() {
+    
+    let socket = new SocketService();
+    await socket.init();
+
+    let x = new EtherscanSearch(socket, "0x263A1C688C4426Eede9B57E741fFb2B9CAdcCA9");
+    
+    let y = await x.process();
+
+    console.log("\n\nBack to sandbox")
+    console.log(y.data);
+}
